7aab6b61066940e0933fafed6b0c1ff9ea8d1472
ducthanhtran <18ducthanh@gmail.com>
2015-03-25 23:41:16 +0100

    Add 1st part of encoding algorithm
    Zeckendorf-representation finished

--- fibonacci_coding.c
+++ fibonacci_coding.c
@@ -18,3 +18,46 @@ unsigned int fibonacciNumber(int n)
     double phi = (1 + sqrt(5.0)) / 2.0;
     return round(pow(phi,n+1) / sqrt(5.0));
 }
+
+
+/*
+ * Sets the k-th bit in num
+ */
+inline void setBit(unsigned int *num, int k)
+{
+    *num |= 1 << k;
+}
+
+
+/*
+ * Encodes a positive integer into a binary codeword with fibonacci numbers as bases
+ * Adding a 1-bit and reversing the codeword yiels a prefix code
+ */
+unsigned int encode_fib(unsigned int N)
+{
+    // find highest fibonacci number that is equal or smaller than N
+    int i = 1;
+    while(fibonacciNumber(++i) <= N);
+    i -= 1;
+    
+    // calculate rest of the Zeckendorf-Representation
+    unsigned int z_repr = 0;
+    while(i > 0)
+    {
+        if(fibonacciNumber(i) <= N)
+        {
+            setBit(&z_repr, i);
+            N -= fibonacciNumber(i);
+            
+            // Zeckendorf-Theorem: there are no consecutive 1-bits
+            i -= 2;
+        }
+        else
+        {
+            i -= 1;
+        }
+    }
+    
+    // TODO: Zeckendorf representation finished; add 1-bit (UD-property) and reverse bit-representation to achieve prefix-code
+    return enc;
+}
